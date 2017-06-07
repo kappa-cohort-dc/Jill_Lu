cbdbeff5ed1b5ba809b087a1e9055b346a9f91e7
Jill Lu <lujx620@gmail.com>
2017-06-07 17:56:46 -0400

    node/express

---
+++ Mean/math_module/app.js
@@ -0,0 +1,6 @@
+var mathlib = require('./mathlib')();
+console.log(mathlib);
+console.log(mathlib.add(6,20));
+console.log(mathlib.multiply(6,20));
+console.log(mathlib.square(6));
+console.log(mathlib.random(1,10));
